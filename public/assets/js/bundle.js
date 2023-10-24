1cb46bdf3c0dc3412bd66f5c9ab26fdbd95650a6
Judson Rian <judson_@outlook.com>
2023-10-23 22:21:53 -0300

    modelo webpack

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// q
+//q
+//
+console.log("BOA TARDE");
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
